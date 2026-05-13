3f1d6b9fd22169b08a5c5d3d404f7794b6c92db6
José Guillermo Ortiz Quispe <ortizquispe1604@gmail.com>
2026-05-12 21:54:44 -0500

    feat: Schemas Personas
    Agregado de campos adicionales para los perfiles de los trbajadores.

--- api/app/schemas/persona.py
+++ api/app/schemas/persona.py
@@ -1,21 +1,24 @@
 from pydantic import BaseModel
 from typing import Literal, Optional
+from datetime import date
 
 class PersonaBase(BaseModel):
     dni: str
     nombre_completo: str
     tipo_trabajador: Literal[1057, 728, 276]
-    is_active: bool = True # Agregado con valor por defecto
+    is_active: bool = True
+    fecha_inicio_labores: date
+    dias_laborables: str = "1,2,3,4,5"
 
 class PersonaCreate(PersonaBase):
     pass
 
-# --- NUEVO ESQUEMA PARA EDITAR ---
 class PersonaUpdate(BaseModel):
-    # Todo es opcional, así puedes actualizar solo el nombre, solo el estado, o ambos
     nombre_completo: Optional[str] = None
     tipo_trabajador: Optional[Literal[1057, 728, 276]] = None
     is_active: Optional[bool] = None
+    fecha_inicio_labores: Optional[date] = None
+    dias_laborables: Optional[str] = None
 
 class Persona(PersonaBase):
     id: int
